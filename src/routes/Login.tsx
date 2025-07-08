d52e61b98b1352cd3c4c4a25b55e40191fddbc86
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-08 11:06:44 -0300

    Diseño del login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex justify-center items-center min-h-screen px-4">
+        <form className="w-full sm:w-full md:w-1/2 lg:w-2/5 xl:w-1/3 bg-gray-700 p-6 rounded shadow-md">      
+            <div className="mt-2">
+                <label className="block text-green-500 font-bold uppercase" htmlFor="username">Username</label>
+                <input className="bg-white rounded w-full p-2" type="text" id="username" />
+            </div>
+
+            <div className="mt-2">
+                <label className="block text-green-500 font-bold uppercase" htmlFor="password">Password</label>
+                <input className="bg-white rounded w-full p-2" type="password" id="password" />
+            </div>
+
+            <button className="mt-4 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded w-full">
+                Login
+            </button>
+            </form>
+        </div>
   )
 }
 
