fa3a545b908342c75fb153d1aa159fa3466b782d
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-08 21:51:42 -0300

    Agregar navbar al componente App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Register from './routes/Register'
 import Login from './routes/Login'
+import Navbar from './components/Navbar'
 
 function App() {
  
   //import.meta.env.VITE_NAME)
   return (
     <>
+    <Navbar/>
      <Routes>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
