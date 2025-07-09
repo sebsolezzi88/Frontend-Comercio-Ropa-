3d5277aef29f274a75b23c092d5b3036b07f2960
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-09 12:15:24 -0300

    Creacion de componente Categories, Orders y ProductForm

---
+++ src/components/Categories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className='text-green-500'>Categories</div>
+  )
+}
+
+export default Categories
---
+++ src/components/ProductForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductForm = () => {
+  return (
+    <div className='text-green-500'>ProductForm</div>
+  )
+}
+
+export default ProductForm
